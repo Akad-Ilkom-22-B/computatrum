436c6baf50c19434aec9165928f5bacc4ebe8665
elisafitri25 <elisafitri964@gmail.com>
2023-08-10 20:57:43 +0700

    add object java script

---
+++ object.js
@@ -0,0 +1,24 @@
+const Data_login = {
+    id: "1",
+    username: "user123",
+    password: "pass123",
+    akses: "mahasiswa",
+    
+  };
+  console.log(Data_login);
+  
+  const Data_Mahasiswa = {
+    id: "1",
+    nim: "1234567890",
+    nama: "Angga",
+    jurusan: "Ilmu Komputer",
+    program_studi: "S1",
+    angkatan: "2022",
+    jenis_kelamin: "Laki-laki",
+    tahun_akademik: "2022/2023",
+    semester: "2",
+    ipk: "3.75",
+    sks: "120",
+    
+  };
+  console.log(Data_Mahasiswa);
