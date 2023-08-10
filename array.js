682c3d7f14bd4f96320c2ab0569e8702fe084494
elisafitri25 <elisafitri964@gmail.com>
2023-08-10 21:06:50 +0700

    add array java script

---
+++ array.js
@@ -0,0 +1,6 @@
+const Data_login = ["1","user123","pass123", "mahasiswa"];
+  console.log(Data_login);
+  
+const Data_Mahasiswa = ["1","1234567890","Angga","Ilmu Komputer","S1", "2022", "Laki-laki","2022/2023", "2", "3.75","120" ];
+  console.log(Data_Mahasiswa);  
+  
